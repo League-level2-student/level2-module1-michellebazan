aadd41b5c10e12331c3626b3b79f694d4aa19ad9
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-11-06 16:58:48 -0800

    GuestBook

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,7 +1,31 @@
 package intro_to_array_lists;
 
-public class GuestBook {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener {
+	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names".
+	public static void main(String[] args) {
+		
+	}
+	JButton addNames = new JButton("Add Name");
+		JButton viewNames = new JButton("View Names");
+	public void start() {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		panel.add(frame);
+		
+		
+		
+		addNames.addActionListener(this);
+		viewNames.addActionListener(this);
+	}
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
@@ -9,4 +33,19 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	ArrayList<String> names = new ArrayList<String>();
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton buttonClicked = (JButton)e.getSource();
+		if (buttonClicked==addNames) {
+			names.add(JOptionPane.showInputDialog("enter a name"));
+			}else if(buttonClicked == viewNames) {
+			//System.out.println(names[]);
+				//https://central.jointheleague.org/levels/Level2/Mod1Recipes/index.html
+				//print arrays out in a ordered fashion
+		}
+	}
+	
+	
 }
